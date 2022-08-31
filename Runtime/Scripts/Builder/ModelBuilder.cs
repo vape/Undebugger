73e494187d192c3aea3de239ea95c5df93f47a96
Artem Sokolov <sokolov.artem@hotmail.com>
2022-08-31 17:37:10 +0300

    Ignore some assemblies when searching for static menu handlers

--- Runtime/Scripts/Builder/ModelBuilder.cs
+++ Runtime/Scripts/Builder/ModelBuilder.cs
@@ -9,6 +9,18 @@ namespace Deszz.Undebugger.Builder
 {
     public static class ModelBuilder
     {
+        private static readonly string[] ignoredAssemblies = new string[]
+        {
+            "UnityEngine",
+            "System",
+            "UnityEditor",
+            "Unity",
+            "netstandart",
+            "Microsoft",
+            "mscorelib",
+            "Mono",
+        };
+
         private static List<MethodInfo> staticHandlers;
 
         public static MenuModel Build()
@@ -41,9 +53,26 @@ namespace Deszz.Undebugger.Builder
             staticHandlers = new List<MethodInfo>();
 
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var ignore = false;
 
             for (int i = 0; i < assemblies.Length; ++i)
             {
+                ignore = false;
+
+                for (int k = 0; k < ignoredAssemblies.Length; ++k)
+                {
+                    if (assemblies[i].FullName.StartsWith(ignoredAssemblies[k]))
+                    {
+                        ignore = true;
+                        break;
+                    }
+                }
+
+                if (ignore)
+                {
+                    continue;
+                }
+
                 var types = assemblies[i].GetTypes();
 
                 for (int j = 0; j < types.Length; ++j)
