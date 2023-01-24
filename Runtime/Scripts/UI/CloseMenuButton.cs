669eaceac34dc8e471c0aabc9db8dc4c46b311e2
Artem Sokolov <sokolov.artem@hotmail.com>
2023-01-24 22:20:05 +0300

    Add close menu button

---
+++ Runtime/Scripts/UI/CloseMenuButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Undebugger.UI
+{
+    public class CloseMenuButton : MonoBehaviour
+    {
+        public void TryClose()
+        {
+            if (UndebuggerManager.Instance != null)
+            {
+                UndebuggerManager.Instance.TryDestroyMenu();
+            }
+        }
+    }
+}
