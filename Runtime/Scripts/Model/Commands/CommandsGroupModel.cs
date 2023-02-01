791b1bc4b03653c317a6098af599495244396fb6
Artem Sokolov <sokolov.artem@hotmail.com>
2023-02-01 20:01:05 +0300

    Implement chain builder as easier way to add commands to menu

--- Runtime/Scripts/Model/Commands/CommandsGroupModel.cs
+++ Runtime/Scripts/Model/Commands/CommandsGroupModel.cs
@@ -4,7 +4,36 @@ namespace Undebugger.Model.Commands
 {
     public class CommandsGroupModel : IGroupModel
     {
+        public const string GlobalPageName = "Global";
+        public const int GlobalPagePriority = 1000;
+
         public List<PageModel> Pages = new List<PageModel>();
+
+        public PageModel GetGlobalPage()
+        {
+            return FindOrCreatePage(GlobalPageName, GlobalPagePriority);
+        }
+
+        public PageModel FindOrCreatePage(string name, int priority)
+        {
+            for (int i = 0; i < Pages.Count; ++i)
+            {
+                if (Pages[i].Name == name)
+                {
+                    return Pages[i];
+                }
+            }
+
+            var page = new PageModel()
+            {
+                Name = name,
+                Priority = priority
+            };
+
+            Pages.Add(page);
+
+            return page;
+        }
 
         public void Sort()
         {
--- Runtime/Scripts/Model/Commands/PageModel.cs
+++ Runtime/Scripts/Model/Commands/PageModel.cs
@@ -4,10 +4,34 @@ namespace Undebugger.Model.Commands
 {
     public class PageModel : IPrioritized
     {
+        public const string MainSegmentName = "Main";
+        public const int MainSegmentPriority = 1000;
+
         public int Priority
         { get; set; }
 
         public string Name;
         public List<SegmentModel> Segments = new List<SegmentModel>();
+
+        public SegmentModel FindOrCreateSegment(string name, int priority)
+        {
+            for (int i = 0; i < Segments.Count; ++i)
+            {
+                if (Segments[i].Name == name)
+                {
+                    return Segments[i];
+                }
+            }
+
+            var segment = new SegmentModel()
+            {
+                Name = name,
+                Priority = priority
+            };
+
+            Segments.Add(segment);
+
+            return segment;
+        }
     }
 }
